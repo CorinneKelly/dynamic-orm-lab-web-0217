51e0978f430a6c7572795b2eb8573c6918d47d51
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-15 22:05:22 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source "https://rubygems.org"
 
 gem 'pry'
 gem 'sqlite3'
+gem 'rspec'
+# gem 'active_support'
+# gem 'active_support'
--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,32 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
+# require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+	
+	def initialize(options={})
+		options.each do |key, value|
+			self.send("#{key}=", value)
+		end
+	end
+
+	def self.table_name
+		"#{self.name.downcase}s"
+		# prob dont need downcase since sql doesnt recognize case
+	end
+
+	def self.column_names
+		DB[:conn].results_as_hash = true
+
+		sql = "PRAGMA table_info('#{table_name}')"
+		table_info = DB[:conn].execute(sql)
+		column_names = []
+		table_info.each do |row|
+			column_names << row["name"]
+		end
+		column_names.compact
+	end
+
+
+
 end
